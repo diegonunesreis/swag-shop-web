7799e8480179ce40fbd1fdd56aeb238268d952b8
Diego Nunes Reis <diegonunesftw@gmail.com>
2018-03-15 01:02:32 -0300

    more chages

--- swag-shop-web/src/App/App.js
+++ swag-shop-web/src/App/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import HttpService from '../services/http-service';
+import Product from '../product/product';
 
 const http = new HttpService();
 
@@ -9,7 +10,19 @@ class App extends Component {
   
   constructor(props){
     super(props);
-    http.getProducts();
+
+    //Bind functions
+    this.loadData = this.loadData.bind(this);
+
+    this.loadData();
+  }
+
+  loadData = () => {
+    http.getProducts().then(products => {
+      console.log(products)
+    }, err =>{
+
+    });
   }
 
   render() {
@@ -19,9 +32,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to Swag Shop</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-main">
+          <Product price="4.23" title="Cool Toy Gun" imgUrl="https://sad.hasbro.com/d4385acb7ccfebf5743d81a3bc180b6678fbad68/07929145dd531a9b19f4fed65a190937.png"/>
+        </div>
       </div>
     );
   }
